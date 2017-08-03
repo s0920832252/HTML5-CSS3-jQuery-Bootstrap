5360536d0f886bac97a422e878214ccf524ff8b4
s0920832252 <s0920832252s@gmail.com>
2017-08-03 22:33:59 +0800

    履歷作業

---
+++ autobiography/js/main.js
@@ -0,0 +1,22 @@
+// 當畫面元素載入完成後，執行以下...
+$(function () {
+    // item被點時 切換內容.
+    $('.navList1').click(function () {
+        $("section.content").load("personalData.html");
+    });
+    $('.navList2').click(function () {
+        $("section.content").load("experience.html");
+    });
+    $('.navList3').click(function () {
+        $("section.content").load("jobRequess.html");
+    });
+    $('.navList4').click(function () {
+        $("section.content").load("skill.html");
+    });
+    $('.navList5').click(function () {
+        $("section.content").load("autobigography.html");
+    });
+    $('.navList6').click(function () {
+        $("section.content").load("recommend.html");
+    });
+});
